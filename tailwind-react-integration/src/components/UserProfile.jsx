943fd75c41bdf82a698091e402a8f82e5fb270cd
chereolisa <justincherechukwu@gmail.com>
2026-02-07 12:18:21 +0100

    tailwind second commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
     <div
       className="
         bg-gray-100 
-        p-4 md:p-8 
+        p-4 md:p-8 sm:p-4
         max-w-xs md:max-w-sm 
         mx-auto my-20 
         rounded-lg 
